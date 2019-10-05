54c75b775b5b4ce2592970ac3be152f7578b56c3
robhyrk <rob.hyrkiel@gmail.com>
2019-10-05 18:14:55 -0400

    Updates

---
+++ precache-manifest.2f5578fd7334676673fa476f74d2c439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31998e2e13946f2ea7919a46d9241aa2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1d5e076170f39ce66d71",
+    "url": "/monster-rolodex/static/css/main.39bc3142.chunk.css"
+  },
+  {
+    "revision": "95dd95d603ec960dd8fc",
+    "url": "/monster-rolodex/static/js/2.fe409418.chunk.js"
+  },
+  {
+    "revision": "1d5e076170f39ce66d71",
+    "url": "/monster-rolodex/static/js/main.bb5e4b1b.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monster-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
