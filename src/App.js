39e3b66d582c3940dcbc8ecc4bdfe9157866a9b5
robhyrk <rob.hyrkiel@gmail.com>
2019-10-05 17:56:13 -0400

    feat: added live serach

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search.box.component'
 import logo from './logo.svg';
 import './App.css';
+import { fromEventPattern } from 'rxjs';
 
 function App() {
   const [name, setName] = useState({
-    monsters: []
+    monsters: [],
+  });
+  const [search, setSearch] = useState({
+    searchField: ''
   });
 
   useEffect( ()=> {
@@ -13,10 +18,22 @@ function App() {
     .then(response => response.json())
     .then(users => setName({monsters:users}))
   }, [])
+
+  const filteredMonsters = name.monsters.filter(monster => 
+    monster.name.toLowerCase().includes(search.searchField.toLowerCase())
+  )
+
+  const handleChange = (e) => {
+    setSearch({searchField: e.target.value})
+  }
   
   return (
     <div className="App">
-      <CardList monsters={name.monsters}/>
+      <SearchBox 
+        placeholder="search monsters"
+        handleChange={handleChange} 
+        />
+      <CardList monsters={filteredMonsters}/>
     </div>
   );
 }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,6 @@ import './card.style.css';
 export const Card = props => (
     <div className="card-container">
         <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
-        {console.log(props)}
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
     </div>
